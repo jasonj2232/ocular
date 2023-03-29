0e1a73328c2720b706ff94642fecede9ccf25b51
Jason Devadoss <jasonj2232@gmail.com>
2023-03-30 05:17:46 +0530

    Changed timer functionality

--- Core/blinkDetector.py
+++ Core/blinkDetector.py
@@ -6,18 +6,24 @@ def euclideanDistance(firstPoint, secondPoint):
     pointOneX, pointOneY = firstPoint
     pointTwoX, pointTwoY = secondPoint
     result = math.sqrt(((pointOneX - pointTwoX)**2) + ((pointOneY - pointTwoY)**2))
-    return result
+    return result * 1000
 
 def noOfBlinks(leftEye_2d_list, rightEye_2d_list):
-    totalTime, leftBlinks, rightBlinks = 3, 0, 0
-    while totalTime != 0:
+    flag = 0
+    startTime = time.time()
+    startSeconds = int(startTime % 60)
+
+    while flag == 0:
+        endTime = time.time()
+        endSeconds = int(endTime % 60)
         leftEyeDist = euclideanDistance(leftEye_2d_list[0], leftEye_2d_list[1])
         rightEyeDist = euclideanDistance(rightEye_2d_list[0], rightEye_2d_list[1])
-        if leftEyeDist < 7:
+        if leftEyeDist < 1000:
             leftBlinks += 1
-        if rightEyeDist < 7:
+        if rightEyeDist < 1000:
             rightBlinks += 1
-        time.sleep(1)
-        totalTime -= 1
+        if endSeconds - startSeconds == 3:
+            break
     return tuple((leftBlinks, rightBlinks))
+
 
